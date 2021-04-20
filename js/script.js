5333b9065bef011b9cb6fca208ecc7d3a2171012
Michele Lorusso <kirito102095@hotmail.it>
2021-04-20 13:41:41 +0200

    Struttura Base

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Descrizione:
+// Rifare l'esercizio dello slider come fatto assieme in classe.
+// Bonus:
+// Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+// Ho allegato lo zip con lo screenshot e le immagini da utilizzare.
